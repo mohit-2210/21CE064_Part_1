27123e610b3d9510222f1d1fd7a335431872473e
Mohit Maheshwari <21CE064@charusat.edu.in>
2022-08-06 21:00:06 +0530

    Create P2_Q5.java

---
+++ P2_Q5.java
@@ -0,0 +1,12 @@
+class Add{  
+    static int add(int a, int b){return a+b;}  
+    static double add(double a, double b){return a+b;}
+    Sting add(string a, string b){return a+b;}  
+}  
+class Overloading{  
+    public static void main(String[] args){  
+    System.out.println(Add.add(67,67));  
+    System.out.println(Add.add(32.4,12.6));  
+    System.out.println(Add.add("hello","world")
+}
+}  
