de28584d18dfeba50cbbaeaec1a40c826fcb4ba4
Mohit Maheshwari <21CE064@charusat.edu.in>
2022-09-17 14:39:40 +0530

    Create Import.java

---
+++ Import.java
@@ -0,0 +1,8 @@
+import Mypack.Demo;
+public class PackageImport {
+    public static void main(String[] args) {
+        Demo obj = new Demo();
+        obj.method();
+    }
+}
+                                                                            
